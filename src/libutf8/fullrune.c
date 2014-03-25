7d2463a73f4cefc03c59819f7fc5db0c9d5799ef
tkr <thomas.kreuzer@rwth-aachen.de>
2014-03-25 17:28:47 +0100

    The Other Side

---
+++ src/libutf8/fullrune.c
@@ -0,0 +1,20 @@
+#include <u.h>
+#include <utf8.h>
+
+int
+fullrune(int c) {
+  uchar h;
+  int n;
+
+  if(c < RUNE_SELF)
+    return 1;
+  h = (0xFF<<(8-UTF_MAX))&0xFF;
+  for(n = UTF_MAX; n > 1; n--) {
+    if((((uchar)c)&h) == h)
+      break;
+    h <<= 1;
+  }
+  if(n == 1)
+    return 0;
+  return n;
+}
