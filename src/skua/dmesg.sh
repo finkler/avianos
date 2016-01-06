37d7cad3c27fe358e0c6e0f35b8470b19504e26a
finkler <finkler@users.noreply.github.com>
2016-01-06 14:10:13 +0100

    Update dmesg.sh

--- src/skua/dmesg.sh
+++ src/skua/dmesg.sh
@@ -9,11 +9,9 @@ while getopts ":n:" flags
 do
   case $flags in
   n)
-    if [ $OPTARG -lt 7 ]; then
-      if [ $OPTARG -gt 0 ]; then
-        echo $OPTARG > /proc/sys/kernel/printk
-        exit 0
-      fi
+    if [ $OPTARG -lt 7 ] || [ $OPTARG -gt 0 ]; then
+      echo $OPTARG > /proc/sys/kernel/printk
+      exit 0
     fi
     echo "dmesg: invalid level $OPTARG" 1>&2
     exit 1;;
